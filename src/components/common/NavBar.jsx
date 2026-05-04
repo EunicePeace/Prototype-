39d61c01147343ab7fd753c16091143d2a9a92e0
LifeGiver13 <lifefotsing@gmail.com>
2026-05-04 14:50:18 +0100

    Css navbar file case fix

--- src/components/common/NavBar.jsx
+++ src/components/common/NavBar.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import './Navbar.css';
+import './NavBar.css';
 import { getFromLocalStorage, removeFromLocalStorage } from '../../utils/authUtils';
 
 const Navbar = () => {
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import Navbar from '../components/common/NavBar';
 import './HomePage.css';
 
 const jobListings = [
